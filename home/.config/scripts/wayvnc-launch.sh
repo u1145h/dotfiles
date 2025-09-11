a1f08276959807d1cec9aeed231677423d9fc9ce
Ullash Roy <ullashroy@pm.me>
2025-09-12 00:59:17 +0530

    Updated config

--- home/.config/scripts/wayvnc-launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Wait until the Wayland socket exists
-while [ ! -S "$XDG_RUNTIME_DIR/wayland-1" ]; do
-  sleep 1
-done
-
-exec /usr/bin/wayvnc 0.0.0.0 --render-cursor --password '84938715'
--- home/.config/scripts/wofi-clipboard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cliphist list | wofi --show=dmenu | cliphist decode | wl-copy
-
